bdad310fabe772444d194ff7596a810acb8bcfcb
Ritesh Koushik <riteshkoushik39@gmail.com>
2024-03-31 02:00:42 +0530

    feat: User routes established

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,5 +1,22 @@
 import express from "express";
+import { authenticate } from "../helpers/token.middleware";
+import {
+    handleLogin, handleRegister, handleDeleteAccount, handleEditProfile
+} from "../controllers/user.controller"
 
 const userRouter = express.Router();
+
+// POST : @api/user/login
+userRouter.post("/login", handleLogin);
+
+// POST : @api/user/register
+userRouter.post("/register", handleRegister);
+
+// PATCH : @api/user/editProfile
+userRouter.patch("/editProfile/:username", authenticate, handleDeleteAccount);
+
+// DELETE : @api/user/deleteProfile
+userRouter.delete("/deleteProfile/:userId", authenticate, handleEditProfile);
+
 
 export default userRouter;
