f87e64a9dcc33c376715f01754fbc0af28850014
Ritesh Koushik <riteshkoushik39@gmail.com>
2024-03-31 02:25:28 +0530

    feat: User model DONE

---
+++ src/models/user.model.ts
@@ -0,0 +1,26 @@
+export interface User {
+    firstName: string,
+    lastName: string,
+    email: string,
+    projects: number,
+    password: string,
+    createdAt: Date,
+    updatedAt: Date,
+    deletedAt: Date,
+    accountStatus: number
+}
+
+// Whenever there is an edit of user profile, the last updated time 
+// needs to be updated as well. For this, the following type is created
+export type NameUpdateModel = {
+    firstName: string,
+    lastName: string,
+    updatedAt: Date
+}
+
+export type DeleteUserModel = {
+    updatedAt: Date,
+    deletedAt: Date,
+    projects: number,
+    accountStatus: number
+}
