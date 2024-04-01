23f3ecdf57d0b3ea7d4be59185a8879f2f92e6a5
Ritesh Koushik <riteshkoushik39@gmail.com>
2024-04-01 10:07:59 +0530

    fix: JWT Claims updated

--- src/helpers/token.generate.ts
+++ src/helpers/token.generate.ts
@@ -3,11 +3,12 @@ import jwt, { Secret } from "jsonwebtoken";
 const SECRET_KEY: Secret = process.env.SECRET_KEY ?? "your-secret-key-here";
 
 interface claims {
-    userid: string,
-    username : string
+    email: string,
+    firstName: string,
+    lastName: string,
 }
 
-export default function newToken(payload: claims, days: number): string {
+export function newToken(payload: claims, days: number): string {
     const token = jwt.sign(payload, SECRET_KEY, {
         expiresIn: `${days} days`
     });
